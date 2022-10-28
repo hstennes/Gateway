604f5630ee1811b7eca666eb9fdda1dc5619ca7d
Hank Stennes <hpstennes@gmail.com>
2022-10-27 23:33:57 -0400

    Labels for connections on custom components

--- src/com/logic/util/CustomHelper.java
+++ src/com/logic/util/CustomHelper.java
@@ -1,6 +1,10 @@
 package com.logic.util;
 
+import com.logic.components.Connection;
 import com.logic.components.LComponent;
+import com.logic.components.Light;
+import com.logic.components.Switch;
+import com.logic.ui.CompProperties;
 
 import java.awt.*;
 import java.awt.font.FontRenderContext;
@@ -22,6 +26,11 @@ public class CustomHelper {
 	 * How far connections extend away from the component
 	 */
 	private final int connectionLength = 25;
+
+	/**
+	 * Value to be added to absolute minimum width required to fit left connection, right connection, main labels
+	 */
+	private final int labelWidthPadding = 50;
 	
 	/**
 	 * The minimum size for the width or height of a component.
@@ -32,6 +41,18 @@ public class CustomHelper {
 	 * The map of lights and switches from the CustomCreator
 	 */
 	private LComponent[][] content;
+
+	private String[][] connectionLabels;
+
+	private int[][] inIndexToSideNum;
+
+	private int[][] outIndexToSideNum;
+
+	private int numInputs;
+
+	private int numOutputs;
+
+	private final int[] sideRange = {Constants.RIGHT, Constants.DOWN, Constants.LEFT, Constants.UP};
 	
 	/**
 	 * Constructs a new CustomHelper
@@ -39,6 +60,15 @@ public class CustomHelper {
 	 */
 	public CustomHelper(LComponent[][] content) {
 		this.content = content;
+		connectionLabels = generateConnectionLabels();
+		for(int i : sideRange) {
+			for(int j = 0; j < content[i].length; j++) {
+				LComponent lcomp = content[i][j];
+				if (lcomp instanceof Switch) numInputs++;
+				else if (lcomp instanceof Light) numOutputs++;
+			}
+		}
+		generateIndexToSideNum();
 	}
 	
 	/**
@@ -49,11 +79,23 @@ public class CustomHelper {
 	 * @return The width of the Custom component
 	 */
 	public int chooseWidth(String label, Font labelFont) {
-		int firstDimension = findDimension(content[Constants.UP], content[Constants.DOWN]);
+		int widthFromConnect = findDimension(content[Constants.UP], content[Constants.DOWN]);
 		FontRenderContext frc = new FontRenderContext(labelFont.getTransform(), true, true);
-		Rectangle2D bounds = labelFont.getStringBounds(label, frc);
-		int secondDimension = (int) (Math.floor(bounds.getWidth()) + 20);
-		return Math.max(firstDimension, secondDimension);
+		Rectangle2D mainLabelBounds = labelFont.getStringBounds(label, frc);
+
+		int maxLeftWidth = 0;
+		for(int i = 0; i < connectionLabels[Constants.LEFT].length; i++) {
+			int width = (int) labelFont.getStringBounds(connectionLabels[Constants.LEFT][i], frc).getWidth();
+			if(width > maxLeftWidth) maxLeftWidth = width;
+		}
+		int maxRightWidth = 0;
+		for(int i = 0; i < connectionLabels[Constants.RIGHT].length; i++){
+			int width = (int) labelFont.getStringBounds(connectionLabels[Constants.RIGHT][i], frc).getWidth();
+			if(width > maxRightWidth) maxRightWidth = width;
+		}
+
+		int widthFromLabels = (int) (mainLabelBounds.getWidth() + Math.max(maxLeftWidth, maxRightWidth) * 2 + labelWidthPadding);
+		return Math.max(widthFromConnect, widthFromLabels);
 	}
 	
 	/**
@@ -90,6 +132,66 @@ public class CustomHelper {
 		}
 		return points;
 	}
+
+	/**
+	 * Returns a 2D array of connection labels in the same format as the content array based on the names of the input / output
+	 * components.
+	 * @return The connection labels
+	 */
+	public String[][] getConnectionLabels() {
+		return connectionLabels;
+	}
+
+	/**
+	 * Returns the side number and position on the side of the connection with the given index in IOManager. Resulting array is
+	 * of the form {side number, index on side}
+	 * @param index The index of the connection in IOManager
+	 * @param connectionType The type of connection (input or output)
+	 * @return The side number and position
+	 */
+	public int[] getSideAndNum(int index, int connectionType) {
+		if(connectionType == Connection.INPUT) return inIndexToSideNum[index];
+		return outIndexToSideNum[index];
+	}
+
+	/**
+	 * Generates a 2D array of connection labels in the same format as the content array based on the names of the input / output
+	 * components.
+	 * @return The connection labels
+	 */
+	private String[][] generateConnectionLabels(){
+		String[][] labels = new String[4][];
+		for(int i : sideRange){
+			labels[i] = new String[content[i].length];
+			for(int j = 0; j < content[i].length; j++) {
+				String compName = content[i][j].getName();
+				labels[i][j] = compName.equals(CompProperties.defaultName) ? "" : compName;
+			}
+		}
+		return labels;
+	}
+
+	/**
+	 * Fills inIndexToSideNum and outIndexToSideNum. See getSideAndNum.
+	 */
+	private void generateIndexToSideNum() {
+		inIndexToSideNum = new int[numInputs][];
+		outIndexToSideNum = new int[numOutputs][];
+		int inIndex = 0;
+		int outIndex = 0;
+		for(int i : sideRange) {
+			for (int j = 0; j < content[i].length; j++) {
+				LComponent lcomp = content[i][j];
+				if (lcomp instanceof Switch) {
+					inIndexToSideNum[inIndex] = new int[]{i, j};
+					inIndex++;
+				} else if (lcomp instanceof Light) {
+					outIndexToSideNum[outIndex] = new int[]{i, j};
+					outIndex++;
+				}
+			}
+		}
+	}
 	
 	/**
 	 * Finds the large pixel dimension required in order to fit the given lists of components on the top/bottom or the left/right
@@ -113,5 +215,4 @@ public class CustomHelper {
 		if(side != null) return side.length;
 		return 0;
 	}
-
 }
